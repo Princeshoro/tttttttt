b85f7fd8eadd77d8f6a5d9fc1d7f7dbb08fe08ae
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-02-12 08:45:07 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/tools-banchat.js
+++ plugins/tools-banchat.js
@@ -1,11 +1,11 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = true
-m.reply(`${eg}\n*🔴Tʜᴇ ʙᴏᴛ ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ𒁂*`)
+m.reply(`${eg}*🔴Tʜᴇ ʙᴏᴛ ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ𒁂*`)
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
 handler.command = /^banchat|bangata|offmd|banchat2$/i
 handler.botAdmin = false
 handler.admin = true 
-handler.owner = true
+handler.rowner = true
 export default handler
