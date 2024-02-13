f554aae4ffecd227cd8d2db115672947f3ca0ea1
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-02-13 09:55:57 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -9,26 +9,26 @@ let isClose = { // Switch Case Like :v
 'cerrar': 'announcement',
 }[(args[0] || '')]
 if (isClose === undefined)
-throw `
-${lenguajeGB'smsAvisoMG'}*╭━[ ${wm} ]━⬣*
-*┃➥ ${usedPrefix + command} open*
+throw `${lenguajeGB['smsAvisoMG']()}*╭━[ ${wm} ]━⬣*
 *┃➥ ${usedPrefix + command} close*
+*┃➥ ${usedPrefix + command} open*
 *╰━━━━━[ 💗 ${vs} ]━━━━━⬣*
 `.trim()
 await conn.groupSettingUpdate(m.chat, isClose)
   
 if (isClose === 'not_announcement'){
-conn.sendButton(m.chat, `${lenguajeGB'smsAvisoEG'}𝙔𝙊𝙐 𝘾𝘼𝙉 𝙒𝙍𝙄𝙏𝙀 𝙀𝙑𝙀𝙍𝙔𝙊𝙉𝙀 𝙄𝙉 𝙏𝙃𝙄𝙎 𝙂𝙍𝙊𝙐𝙋!!`, `𝙂𝙍𝙊𝙐𝙋 𝙊𝙋𝙀𝙉\n${wm}`, pp, [['𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', `.cuentasgb`], ['𝙂𝙤 𝙗𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `/menu`]], m)
+m.reply(`${lenguajeGB['smsAvisoEG']()}${mid.smsGrup}`)
 }
   
 if (isClose === 'announcement'){
-//m.reply(`${eg}*𝙂𝙍𝙊𝙐𝙋 𝘾𝙇𝙊𝙎𝙀𝘿*\n𝙊𝙉𝙇𝙔 𝘼𝘿𝙈𝙄𝙉𝙎 𝘾𝘼𝙉 𝙒𝙍𝙄𝙏𝙀 𝙄𝙉 𝙏𝙃𝙄𝙎 𝙂𝙍𝙊𝙐𝙋`)
-conn.sendButton(m.chat, `${lenguajeGB'smsAvisoEG'}𝙊𝙉𝙇𝙔 𝘼𝘿𝙈𝙄𝙉𝙎 𝘾𝘼𝙉 𝙒𝙍𝙄𝙏𝙀 𝙄𝙉 𝙏𝙃𝙄𝙎 𝙂𝙍𝙊𝙐𝙋!!`, `𝙂𝙍𝙊𝙐𝙋 𝘾𝙇𝙊𝙎𝙀𝘿\n${wm}`, pp, [['𝘼𝙙𝙢𝙞𝙣 𝙈𝙤𝙢𝙚𝙣𝙩 😎', '.s'], ['𝙂𝙤 𝙗𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `/menu`]], m)
-}  
- }
-handler.help = ['group open / close', 'group open / close']
+m.reply(`${lenguajeGB['smsAvisoEG']()}${mid.smsGrup}`)
+}}
+handler.help = ['group open / close', 'grupo abrir / cerrar']
 handler.tags = ['group']
-handler.command = /^(group|gp)$/i
+handler.command = /^(group|grupo)$/i
 handler.admin = true
 handler.botAdmin = true
 export default handler
+
+
+//conn.sendButton(m.chat, `${lenguajeGB['smsAvisoEG']()}𝙔𝘼 𝙋𝙐𝙀𝘿𝙀𝙉 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝙏𝙊𝘿𝙊𝙎 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊!!`, `𝙂𝙍𝙐𝙋𝙊 𝘼𝘽𝙄𝙀𝙍𝙏𝙊\n${wm}`, pp, [['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', `.cuentasgb`], ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `/menu`]], m)
