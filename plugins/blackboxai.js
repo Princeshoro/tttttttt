c092c1fdfd84f726ed4db2857a6a953a4139f6d2
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-03-04 08:06:12 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/blackboxai.js
+++ plugins/blackboxai.js
@@ -3,24 +3,26 @@ import fetch from 'node-fetch';
 let handler = async (m, { text, usedPrefix, command }) => {
   
   if (!text && !(m.quoted && m.quoted.text)) {
-    m.reply(`*${lenguajeGB['smsAvisoMG']()}⚫𝙀𝙓𝘼𝙈𝙋𝙇𝙀: *${usedPrefix + command}* Hi😊 it's blackboxai you ask about code or your question??`);
-    return;
+    return m.reply(`*${lenguajeGB['smsAvisoMG']()}⚫𝙀𝙓𝘼𝙈𝙋𝙇𝙀: *${usedPrefix + command}* Hi😊 it's blackboxai you ask about code or your question??`);
   }
 
-  // Use text or quoted text as a prompt
   let prompt = text || m.quoted.text;
 
   try {
     m.react("⏳");
     let response = await fetch(`https://vihangayt.me/tools/blackboxv4?q=${encodeURIComponent(prompt)}`);
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
     let data = await response.json();
     let result = data.result;
     m.reply(result);
     m.react("✅");
   } catch (error) {
     console.error('Error:', error); 
-    m.reply(`*ERROR*: ${error}`);
+    m.reply(`*ERROR*: ${error.message}`);
   }
 };
 
