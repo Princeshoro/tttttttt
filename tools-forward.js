f88eac5a8a1ca46c4f206100471dfb4cadbb9a9d
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-03-09 10:11:27 +0500

    Update tools-forward.js

--- tools-forward.js
+++ tools-forward.js
@@ -20,11 +20,11 @@ const handler = async (m, { conn, text, isOwner, isAdmin }) => {
     const htextos = `${text ? text : '*Here you go*'}`;
     if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
       var mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, this.copyNForward { image: mediax, caption: htextos }, { quoted: m });
+      conn.sendMessage(m.chat, { image: mediax, caption: htextos }, { quoted: m });
       m.react('✅')
     } else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
       var mediax = await quoted.download?.();
-      conn.sendMessage(m.chat { video: mediax, mimetype: 'video/mp4', caption: htextos }, { quoted: m });
+      conn.sendMessage(m.chat, { video: mediax, mimetype: 'video/mp4', caption: htextos }, { quoted: m });
      
       m.react('✅')
     } else {
@@ -32,7 +32,7 @@ const handler = async (m, { conn, text, isOwner, isAdmin }) => {
     }
   }
 };
-handler.command = /^(forward|save)$/i;
+handler.command = /^(frd)$/i;
 handler.tags = ['tools'];
 handler.group = false;
 handler.admin = false;
