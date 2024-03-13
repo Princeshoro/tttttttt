a15857d5e787a635b2d219cc552159dc6b21744a
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-03-13 23:58:05 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/gemini-ai.js
+++ plugins/gemini-ai.js
@@ -12,7 +12,7 @@ let handler = async (m, { text, usedPrefix, command }) => {
   try {
     m.react("⏳");
 
-    let response = await fetch(`https://vihangayt.me/tools/gemini?q=${encodeURIComponent(text)}`);
+    let response = await fetch(`https://api.vihangayt.asia/ai/gemini?q=${encodeURIComponent(text)}`);
     const data = await response.json();
     let result = data.data || "*GEMINI-AI API ERROR TRY LATER*";
     m.reply(result);
