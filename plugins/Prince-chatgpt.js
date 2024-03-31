05bb4e127194dea87618ea5563f8fd5fa13bd15b
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-03-31 08:26:28 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/Prince-chatgpt.js
+++ plugins/Prince-chatgpt.js
@@ -1,42 +1,50 @@
-import fetch from 'node-fetch';
+
+import displayLoadingScreen from '../lib/loading.js'
+import fetch from 'node-fetch'
 import {delay} from '@whiskeysockets/baileys'
 
-let handler = async (m, { text, usedPrefix, command }) => {
-  
-  // Check if the text or quoted text is provided
-  if (!text && !(m.quoted && m.quoted.text)) {
-    // Send a message to the user asking for input
-    m.reply(`Please provide some text or quote a message to get a response.`);
-    // Exit the function
-    return;
-  }
-
-  // Use the text or quoted text as the prompt
-  let prompt = text || m.quoted.text;
-
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   try {
-    // React with a heart emoji
-    m.react("⏳")
-    // Fetch the response from the API
-    const response = await fetch(`https://ultimetron.guruapi.tech/gpt4?prompt=${prompt}`);
-    // Parse the response as JSON
-    const data = await response.json();
-   // Get the completion from the data
-   let result = data.completion || "GPT4 SERVER ERROR";
-  // Reply with the result
-  m.reply(result.trim());
+    if (!text) throw `uhm.. what do you want to say?`
+    m.react('⏳')
+    //await displayLoadingScreen(conn, m.chat)
 
-    // React with a pointing down emoji
-    m.react("✅")
+
+    const prompt = encodeURIComponent(text);
+    let apiurl = `https://ultimetron.guruapi.tech/gpt4?prompt=${prompt}`
+
+    const result = await fetch(apiurl);
+    const response = await result.json();
+    console.log(response)
+    const textt = response.result.reply;
+    await typewriterEffect(conn,m, m.chat , textt);
+       m.react('✅')
   } catch (error) {
-    // Log the error
-    console.error('Error:', error); 
-    // Reply with an error message
-    m.reply(`*ERROR*: ${error.message}`);
+    console.error(error);
+    m.reply('Oops! Something went wrong. , we are trying had to fix it asap');
   }
-};
+}
+handler.help = ['gpt4 <text>']
+handler.tags = ['tools']
+handler.command = /^(gpt4|gds)$/i
 
-handler.command = ['gpt4', 'gds'];
-handler.diamond = false;
+export default handler
 
-export default handler;
+async function typewriterEffect(conn, quoted ,from, text) {
+    let { key } = await conn.sendMessage(from, { text: 'Thinking...' } , {quoted:quoted})
+  
+    for (let i = 0; i < text.length; i++) {
+      const noobText = text.slice(0, i + 1);
+      await conn.relayMessage(from, {
+        protocolMessage: {
+          key: key,
+          type: 14,
+          editedMessage: {
+            conversation: noobText
+          }
+        }
+      }, {});
+   
+       await delay(100); // Adjust the delay time (in milliseconds) as needed
+    }
+  }
