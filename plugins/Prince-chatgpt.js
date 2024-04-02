feaf48d626efca8aaaea18884183f270568f53d6
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-04-02 10:27:13 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/Prince-chatgpt.js
+++ plugins/Prince-chatgpt.js
@@ -1,6 +1,6 @@
-import displayLoadingScreen from '../lib/loading.js'
-import fetch from 'node-fetch'
-import { delay } from '@whiskeysockets/baileys'
+import displayLoadingScreen from '../lib/loading.js';
+import fetch from 'node-fetch';
+import { delay } from '@whiskeysockets/baileys';
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
     if (!text && !(m.quoted && m.quoted.text)) {    
@@ -9,18 +9,18 @@ if (!text) throw `*${lenguajeGB['smsAvisoMG']()}🧊𝙀𝙓𝘼𝙈𝙋𝙇𝙀
   if (!text && m.quoted && m.quoted.text) {
     text = m.quoted.text;
   }
-try {
-    m.react('⏳')
-    //await displayLoadingScreen(conn, m.chat)
+  try {
+    m.react('⏳');
+    await displayLoadingScreen(conn, m.chat);
 
     const prompt = encodeURIComponent(text);
     let apiurl = `https://ultimetron.guruapi.tech/gpt4?prompt=${prompt}`
 
     const result = await fetch(apiurl);
     const response = await result.json();
-    console.log(response)
+    console.log(response);
     const textt = response.result.reply;
-    await typewriterEffect(conn, m.chat, textt);
+    await conn.sendMessage(m.chat, { text: textt });
 
   } catch (error) {
     console.error(error);
@@ -32,12 +32,4 @@ handler.help = ['gemini <text>']
 handler.tags = ['tools']
 handler.command = /^(gpt4)$/i
 
-export default handler
-
-async function typewriterEffect(conn, chatId, text) {
-  for (let i = 0; i < text.length; i++) {
-    const noobText = text.slice(0, i + 1);
-    await conn.sendMessage(chatId, { text: noobText });
-    await delay(100); // Adjust the delay time (in milliseconds) as needed
-  }
-}
+export default handler;
