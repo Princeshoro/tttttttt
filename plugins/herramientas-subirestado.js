76df33ac212ae4544f727224d963c4c803094927
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-04-14 06:33:50 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/herramientas-subirestado.js
+++ plugins/herramientas-subirestado.js
@@ -13,7 +13,7 @@ const colors = [
 ] 
 let handler = async (m, { conn, text }) => {
     let _m = Promise.resolve({ key: { id: '' }})
-    if (!m.quoted && !text) throw '*_Ingrese un texto o reponda a algun archivo multimedia_*'
+    if (!m.quoted && !text) throw '*_Enter a text or reply to a multimedia file_*'
     if (m.quoted && m.quoted.mtype !== 'conversation' && !text) _m = m.quoted.forward('status@broadcast')
     if (m.quoted && m.quoted.mtype === 'conversation' && !text) _m = conn.sendMessage('status@broadcast', {
         text: m.quoted.text,
@@ -27,11 +27,11 @@ let handler = async (m, { conn, text }) => {
     }, 'extendedTextMessage')
     if (m.quoted && text) _m = conn.forwardMessage('status@broadcast', await m.quoted.cMod('status@broadcast', text))
     //m.reply((await _m).key.id)
-    m.reply('*✅ Estado subido con exito, agenda en tu lista de contactos el número del Bot y pide al propietario del Bot que te agregue a sus contactos para que puedas ver los estados*')
+    m.reply('*✅ Status uploaded successfully, schedule the Bot number in your contact list and ask the Bot owner to add you to their contacts so you can see the statuses*')
     }
-handler.help = ['subirestado']
+handler.help = ['statusup']
 handler.tags = ['General']
-handler.command = /^subirestado$/i
+handler.command = /^statusup$/i
 handler.register = false
 export default handler
 
