c8d4c4520de65952c076897f5b58b4e1b856a97f
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-04-15 18:55:44 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/dl-tiktokA.js
+++ plugins/dl-tiktokA.js
@@ -4,39 +4,43 @@ import cheerio from 'cheerio'
 import { tiktok } from "@xct007/frieren-scraper";
 let generateWAMessageFromContent = (await import(global.baileys)).default
 import { tiktokdl } from '@bochilteam/scraper'
-let handler = async (m, { conn, text, args, usedPrefix, command}) => {
- if (!text) throw `_*PRINCE TIKTOK DL*_\n\n*_Past a tiktok link._*\n\n*_Example:_* _${usedPrefix + command} Url here_`;
-  if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw `_*PRINCE TIKTOK DL*_\n\n*_Enter a TikTok link._*\n\n*_Example:_* _${usedPrefix + command} Url here_`;
-  
-  
-  try {
-const dataF = await tiktok.v1(args[0])
-conn.sendFile(m.chat, dataF.play, 'tiktok.mp4', `⛱️ ${mid.user}\n*${nickname}*\n${description ? '\n⛱️ ${mid.smsYT14}\n*${description}*' : ''}\n${wm}`.trim(), m) 
-} catch (e1) {
-try {
-const tTiktok = await tiktokdlF(args[0])
-conn.sendFile(m.chat, tTiktok.video, 'tiktok.mp4', `⛱️ ${mid.user}\n*${nickname}*\n${description ? '\n⛱️ ${mid.smsYT14}\n*${description}*' : ''}\n${wm}`.trim(), m) 
-} catch (e2) {
-try {
-let p = await fg.tiktok(args[0]) 
-conn.sendFile(m.chat, p.nowm, 'tiktok.mp4', `⛱️ ${mid.user}\n*${nickname}*\n${description ? '\n⛱️ ${mid.smsYT14}\n*${description}*' : ''}\n${wm}`.trim(), m)
-} catch (e3) {
-try { 
-const { author: { nickname }, video, description } = await tiktokdl(args[0])
-const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-conn.sendFile(m.chat, url, 'tiktok.mp4', `⛱️ ${mid.user}\n*${nickname}*\n${description ? `\n⛱️ ${mid.smsYT14}\n*${description}*` : ''}\n${wm}`.trim(), m)
 
-} catch (e) {
-await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`,.trim(), m)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-handler.limit = false
-}}}}}
-handler.help = ['tiktok']
-handler.tags = ['dl']
-handler.command = ['ttkk']
-//handler.limit = 2
-export default handler
+let handler = async (m, { conn, text, args, usedPrefix, command}) => {
+  if (!text) throw `_*PRINCE TIKTOK DL*_\n\n*_Past a tiktok link._*\n\n*_Example:_* _${usedPrefix + command} Url here_`;
+  if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw `_*PRINCE TIKTOK DL*_\n\n*_Enter a TikTok link._*\n\n*_Example:_* _${usedPrefix + command} Url here_`;
+
+  try {
+    const dataF = await fg.tiktok(args[0]);
+    conn.sendFile(m.chat, dataF.nowm, 'tiktok.mp4', `⛱️ ${m.sender.username}\n${dataF.author.nickname}\n${dataF.description ? '\n⛱️ ' + dataF.author.uniqueId + '\n' + dataF.description : ''}\n${wm}`.trim(), m);
+  } catch (e1) {
+    try {
+      const tTiktok = await fg.tiktok(args[0]);
+      conn.sendFile(m.chat, tTiktok.nowm, 'tiktok.mp4', `⛱️ ${m.sender.username}\n${tTiktok.author.nickname}\n${tTiktok.description ? '\n⛱️ ' + tTiktok.author.uniqueId + '\n' + tTiktok.description : ''}\n${wm}`.trim(), m);
+    } catch (e2) {
+      try {
+        const p = await fg.tiktok(args[0]);
+        conn.sendFile(m.chat, p.nowm, 'tiktok.mp4', `⛱️ ${m.sender.username}\n${p.author.nickname}\n${p.description ? '\n⛱️ ' + p.author.uniqueId + '\n' + p.description : ''}\n${wm}`.trim(), m);
+      } catch (e3) {
+        try {
+          const { author: { nickname }, video, description } = await fg.tiktok(args[0]);
+          const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
+          conn.sendFile(m.chat, url, 'tiktok.mp4', `⛱️ ${m.sender.username}\n${nickname}\n${description ? '\n⛱️ ' + dataF.author.uniqueId + '\n' + description : ''}\n${wm}`.trim(), m);
+        } catch (e) {
+          await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`.trim(), m);
+          console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`);
+          console.log(e);
+          handler.limit = false;
+        }
+      }
+    }
+  }
+};
+
+handler.help = ['tiktok'];
+handler.tags = ['dl'];
+handler.command = ['ttkk'];
+//handler.limit = 2;
+export default handler;
 
 async function tiktokdlF(url) {
 if (!/tiktok/.test(url)) return 'Enlace incorrecto';
