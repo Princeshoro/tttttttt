a87fd3e1aca54625a0f93660abf9ce950e65d2f3
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-04-24 11:56:18 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/geminiofficial.js
+++ plugins/geminiofficial.js
@@ -10,8 +10,8 @@ try {
   m.react('⏳')
 var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
 var res = await apii.json()
-await m.reply(res.result)
   m.react('✅')
+await m.reply(res.result)
 } catch (e) {
 await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
