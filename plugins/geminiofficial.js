1ce33f276240bc9706f03666e7bf316d904f931e
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-04-24 11:45:29 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/geminiofficial.js
+++ plugins/geminiofficial.js
@@ -1,18 +1,25 @@
 import fetch from 'node-fetch'
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) throw `ENTER A TEXT!`
+  if (!text && !(m.quoted && m.quoted.text)) {
+if (!text) throw `𝙏𝙋𝙔𝙀 .ai 𝘼𝙉𝘿 𝘼𝙎𝙆 𝙔𝙊𝙐𝙍 𝙌𝙐𝙀𝙎𝙏𝙄𝙊𝙉!`
+     }
+  if (!text && m.quoted && m.quoted.text) {
+    text = m.quoted.text;
+  }
 try {
+  m.react('⏳')
 var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
 var res = await apii.json()
 await m.reply(res.result)
+  m.react('✅')
 } catch (e) {
 await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)
 }}
-handler.command = ['gm2', 'gemini2']
-handler.help = ['gm2', 'gemini2']
-handler.tags = ['herramientas']
+handler.command = ['ai']
+handler.help = ['ai']
+handler.tags = ['tools']
 
 handler.premium = false
 
