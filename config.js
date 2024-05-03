4a8474b5ac7b10830e1222ee314a2827cc14869b
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-05-03 19:24:20 +0500

    ɢᴅs-ᴍᴅ

--- config.js
+++ config.js
@@ -115,6 +115,7 @@ global.APIKeys = {
 
 
 
+global.fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
 
 
