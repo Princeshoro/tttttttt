631aabc8932a9a0602e385ad3921b7110a9540ee
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-05-07 07:39:11 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/blackboxai.js
+++ plugins/blackboxai.js
@@ -12,7 +12,7 @@ let handler = async (m, { text, usedPrefix, command }) => {
   try {
     m.react("⏳");
 
-    let response = await fetch(`https://api.vihangayt.asia/ai/blackbox?q=${encodeURIComponent(text)}`);
+    let response = await fetch(`https://api.bk9.site/ai/blackbox?q=${encodeURIComponent(text)}`);
     const data = await response.json();
     let result = data.data || "*BLACKBOXAI API ERROR TRY LATER*";
     m.reply(result);
