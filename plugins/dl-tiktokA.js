ccfff491f2b99403d64bf13e7a15624f4a400486
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-05-19 10:24:32 +0500

    💦ᴘʀɪɴᴄᴇ-ᴍᴅ💦

--- plugins/dl-tiktokA.js
+++ plugins/dl-tiktokA.js
@@ -1,47 +1,49 @@
-import { tiktokdl } from '@bochilteam/scraper'
-import fg from 'api-dylux'
+import { tiktokdl } from '@bochilteam/scraper';
+import fg from 'api-dylux';
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   if (!args[0] && m.quoted && m.quoted.text) {
-    args[0] = m.quoted.text
+    args[0] = m.quoted.text;
   }
-  if (!args[0] && !m.quoted) throw `Give the link of the video Tiktok or quote a tiktok link`
-  if (!args[0].match(/tiktok/gi)) throw `Verify that the link is from TikTok`
+  if (!args[0] &&!m.quoted) {
+    throw `Give the link of the video Tiktok or quote a tiktok link`;
+  }
+  if (!args[0].match(/tiktok/gi)) {
+    throw `Verify that the link is from TikTok`;
+  }
 
-m.react('⏳')
-  
-  m.reply(`⏳ *Wait a moment...*`)
+  m.react('⏳');
+  m.reply(`⏳ *Wait a moment...*`);
 
   try {
-  	
-    const {
-      author: { nickname },
-      video,
-      description,
-    } = await tiktokdl(args[0])
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]);
     const url =
       video.no_watermark2 ||
       video.no_watermark ||
       'https://tikcdn.net' + video.no_watermark_raw ||
-      video.no_watermark_hd
+      video.no_watermark_hd;
 
-    if (!url) throw global.error
-    
+    if (!url) {
+      throw global.error;
+    }
+
     let txt = `${vidcap}`;
-    m.react('✅')
-    conn.sendFile(m.chat, url, 'tiktok.mp4', '', m)
+    m.react('✅');
+    conn.sendFile(m.chat, url, 'tiktok.mp4', '', m);
   } catch (err) {
+    console.error(err);
     try {
-      let p = await fg.tiktok(args[0])
-      conn.sendFile(m.chat, p.play, 'tiktok.mp4', txt, m)
-    } catch {
-           m.reply('*An unexpected error occurred*')
+      let p = await fg.tiktok(args[0]);
+      conn.sendFile(m.chat, p.play, 'tiktok.mp4', txt, m);
+    } catch (err) {
+      console.error(err);
+      m.reply('*An unexpected error occurred*');
     }
   }
-}
+};
 
-handler.help = ['tiktok'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = ['ttkk']
+handler.help = ['tiktok'].map(v => v + 'url>');
+handler.tags = ['downloader'];
+handler.command = ['ttkk'];
 
-export default handler
+export default handler;
