a6257240cc45d4093053f4aa4a656c11166ff9b9
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-07-20 15:42:23 +0500

    Create _contents.js

---
+++ plugins/_contents.js
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch'
+import moment from 'moment-timezone'
+import axios from 'axios'
+import fs from 'fs'
+const { generateWAMessageFromContent, prepareWAMessageMedia, proto }  = (await import(global.baileys))
+
+let handler = m => m
+handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner } ) {
+global.key = ''
+
+
+  
+}
+export default handler
+
+// Mensaje de espera
+// await mensajesEditados(conn, m)
+global.mensajesEditados = async function(conn, m) {
+const mensajes = [waitt, waittt, waitttt, waittttt]
+key = await conn.sendMessage(m.chat, { text: wait, quoted: m })
+for (let i = 0; i < mensajes.length; i++) {
+await new Promise(resolve => setTimeout(resolve, 1000))
+await conn.sendMessage(m.chat, { text: mensajes[i], edit: key })
+}}
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]
+  }
+
+async function getRandomChannel() {
+let randomIndex = Math.floor(Math.random() * canalIdGB.length)
+let id = canalIdGB[randomIndex]
+let nombre = canalNombreGB[randomIndex]
+return { id, nombre }
+} 	
