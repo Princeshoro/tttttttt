0da8a8d03307b574d51cb9abae444a3534369c40
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-07-23 16:00:09 +0500

    Update main.js

--- main.js
+++ main.js
@@ -25,44 +25,16 @@ import store from './lib/store.js'
 import readline from 'readline'
 import NodeCache from 'node-cache'
 import processTxtAndSaveCredentials from './lib/makesession.js';
-const {
-    DisconnectReason,
-    useMultiFileAuthState,
-    MessageRetryMap,
-    fetchLatestBaileysVersion,
-    makeCacheableSignalKeyStore,
-    makeInMemoryStore,
-    proto,
-    delay,
-    jidNormalizedUser,
-    PHONENUMBER_MCC,
-    Browsers
-} = await (await import('@whiskeysockets/baileys')).default;
 
-dotenv.config()
-
-async function main() {
-  const txt = process.env.SESSION_ID; 
-  
-  if (!txt) {
-    console.error("Environment variable not found.");
-    return;
-  }
-  
-  try {
-    await processTxtAndSaveCredentials(txt); // Wait for processTxtAndSaveCredentials to complete
-    console.log("processTxtAndSaveCredentials completed.");
-
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-main();
-
-await delay(1000 * 10)
+const { makeInMemoryStore, 
+       DisconnectReason, 
+       useMultiFileAuthState, 
+       MessageRetryMap, 
+       fetchLatestBaileysVersion, 
+       makeCacheableSignalKeyStore,PHONENUMBER_MCC } = await import('@whiskeysockets/baileys')
 
 const { CONNECTING } = ws
+
 const { chain } = lodash
 const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 protoType()
